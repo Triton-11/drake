a01718913b83efc19fb9ec75ae228bb8851def53
robinsch <robin.scherrer@gmail.com>
2016-10-18 12:35:35 +0200

    minor changes, renamed some variables

--- drake/examples/RobinsExample/test/helloworld_test.cc
+++ drake/examples/RobinsExample/test/helloworld_test.cc
@@ -1,12 +1,13 @@
 #include "gtest/gtest.h"
 
 #include "drake/examples/RobinsExample/library/library_robin.h"
+#include "drake/examples/RobinsExample/library/type_template_methods.h"
 
-GTEST_TEST(testHelloworld, doesReturnSomething) {
-int initial_value = 1;
-TestClass myT(1);
-int v = myT.get_value();
+GTEST_TEST(testHelloworld, compareReturnValue) {
+int initial_value = 12;
+TestClass myInstance(initial_value);
+int return_value = myInstance.get_value();
 
-ASSERT_EQ(initial_value, v);
+ASSERT_EQ(initial_value, return_value);
 }
 
