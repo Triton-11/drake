549b5be825cf5b72d04579257f4bca1a594643ae
robinsch <robin.scherrer@gmail.com>
2017-07-05 15:40:55 +0200

    simplified RigidBodyFrame test

--- drake/multibody/test/rigid_body_frame_compare_to_clone.cc
+++ drake/multibody/test/rigid_body_frame_compare_to_clone.cc
@@ -10,51 +10,8 @@ namespace test {
 namespace rigid_body_frame {
 
 template <typename T>
-bool CompareToClone(const RigidBodyFrame<T>& original,
+bool CompareToClone(const RigidBodyFrame<double>& original,
                   const RigidBodyFrame<T>& clone) {
-  if (original.get_name() != clone.get_name()) {
-    drake::log()->debug(
-        "RigidBodyFrame::CompareToClone(): name mismatch:\n"
-        "  - original: {}\n"
-        "  - clone: {}",
-        original.get_name(),
-        clone.get_name());
-    return false;
-  }
-  if (original.get_rigid_body().get_body_index() !=
-      clone.get_rigid_body().get_body_index()) {
-    drake::log()->debug(
-        "RigidBodyFrame::CompareToClone(): rigid body index mismatch:\n"
-        "  - original: {}\n"
-        "  - clone: {}",
-        original.get_rigid_body().get_body_index(),
-        clone.get_rigid_body().get_body_index());
-    return false;
-  }
-  if (original.get_transform_to_body().matrix() !=
-      clone.get_transform_to_body().matrix()) {
-    drake::log()->debug(
-        "RigidBodyFrame::CompareToClone(): transform_to_body mismatch:\n"
-        "  - original:\n{}\n"
-        "  - clone:\n{}",
-        original.get_transform_to_body().matrix(),
-        clone.get_transform_to_body().matrix());
-    return false;
-  }
-  if (original.get_frame_index() != clone.get_frame_index()) {
-    drake::log()->debug(
-        "RigidBodyFrame::CompareToClone(): frame index mismatch:\n"
-        "  - original: {}\n"
-        "  - clone: {}",
-        original.get_frame_index(),
-        clone.get_frame_index());
-    return false;
-  }
-  return true;
-}
-
-bool CompareToAutoDiffXd(const RigidBodyFrame<double>& original,
-                         const RigidBodyFrame<AutoDiffXd>& clone) {
   if (original.get_name() != clone.get_name()) {
     drake::log()->debug(
         "RigidBodyFrame::CompareToClone(): name mismatch:\n"
@@ -100,7 +57,7 @@ bool CompareToAutoDiffXd(const RigidBodyFrame<double>& original,
 template bool CompareToClone<double>(const RigidBodyFrame<double>& original,
                                      const RigidBodyFrame<double>& clone);
 template bool CompareToClone<AutoDiffXd>(
-    const RigidBodyFrame<AutoDiffXd>& original,
+    const RigidBodyFrame<double>& original,
     const RigidBodyFrame<AutoDiffXd>& clone);
 
 }  // namespace rigid_body_frame
