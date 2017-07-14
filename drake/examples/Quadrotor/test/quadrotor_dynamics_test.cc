aea3137a07dfba294f00c95ad4686e71e386d31c
robinsch <robin.scherrer@gmail.com>
2017-07-14 11:56:05 +0200

    add RBTad model with kQuaternion floating base to passive dynamics unit test

--- drake/examples/Quadrotor/test/quadrotor_dynamics_test.cc
+++ drake/examples/Quadrotor/test/quadrotor_dynamics_test.cc
@@ -5,6 +5,7 @@
 #include "drake/common/drake_path.h"
 #include "drake/common/eigen_matrix_compare.h"
 #include "drake/examples/Quadrotor/quadrotor_plant.h"
+#include "drake/math/quaternion.h"
 #include "drake/multibody/parsers/urdf_parser.h"
 #include "drake/multibody/rigid_body_plant/rigid_body_plant.h"
 #include "drake/multibody/rigid_body_tree_construction.h"
@@ -99,11 +100,11 @@ class RigidBodyQuadrotor: public systems::Diagram<T> {
   systems::RigidBodyPlant<T> *plant_{};
 };
 //  A Quadrotor as a RigidBodyPlant that is created from a model
-// specified in a URDF file.
+// specified in a URDF file using a Euler angles.
 template<typename T>
-class RigidBodyAutoDiffQuadrotor: public systems::Diagram<T> {
+class RigidBodyAutoDiffRPYQuadrotor: public systems::Diagram<T> {
  public:
-  RigidBodyAutoDiffQuadrotor() {
+  RigidBodyAutoDiffRPYQuadrotor() {
     this->set_name("QuadrotorAD");
 
     auto tree = std::make_unique<RigidBodyTree<T>>();
@@ -156,6 +157,80 @@ class RigidBodyAutoDiffQuadrotor: public systems::Diagram<T> {
  private:
   systems::RigidBodyPlant<T> *plant_{};
 };
+//  A Quadrotor as a RigidBodyPlant that is created from a model
+// specified in a URDF file using quaternions
+template<typename T>
+class RigidBodyAutoDiffQuaternionQuadrotor: public systems::Diagram<T> {
+ public:
+  RigidBodyAutoDiffQuaternionQuadrotor() {
+    this->set_name("QuadrotorADQuaternion");
+
+    auto tree = std::make_unique<RigidBodyTree<T>>();
+
+    drake::parsers::urdf::AddModelInstanceFromUrdfFile(
+        drake::GetDrakePath() + "/examples/Quadrotor/quadrotor.urdf",
+        multibody::joints::kQuaternion, nullptr, tree.get());
+
+    // TODO(robinsch): Define B matrix as part of RBPad
+    const double kM = 0.0245;
+    const double kF = 1;
+    const double L = 0.175;
+
+    tree->B.resize(6, 4);
+    tree->B.fill(0.0);
+    // Fill the actuator to body mapping matrix.
+    tree->B.block(0, 0, 3, 4) << 0.0, L*kF, 0.0, -L*kF,
+                                 -L*kF, 0.0, L*kF, 0.0,
+                                 kM, -kM, kM, -kM;
+    tree->B.block(5, 0, 1, 4) << kF, kF, kF, kF;
+
+    systems::DiagramBuilder<T> builder;
+
+    plant_ =
+        builder.template AddSystem<systems::RigidBodyPlantAutodiff<T>>(std::move(tree));
+    plant_->set_name("plantADQuaternion");
+
+    const int num_inputs = (plant_->get_num_input_ports() > 0)
+                           ? plant_->get_input_port(0).size()
+                           : 0;
+
+    VectorX<T> hover_input(num_inputs);
+    hover_input.setZero();
+    systems::ConstantVectorSource<T>* source =
+        builder.template AddSystem<systems::ConstantVectorSource<T>>(
+            hover_input);
+    source->set_name("hover_input");
+
+    builder.Connect(source->get_output_port(), plant_->get_input_port(0));
+
+    builder.BuildInto(this);
+  }
+
+  void SetState(systems::Context<T> *context, VectorX<T> x) const {
+    systems::Context<T> *plant_context =
+        this->GetMutableSubsystemContext(context, plant_);
+    VectorX<T> x_quaternion = ConvertRPYStateToQuaternion(x);
+    plant_->set_state_vector(plant_context, x_quaternion);
+  }
+
+ private:
+  systems::RigidBodyPlant<T> *plant_{};
+
+  VectorX<double> ConvertRPYStateToQuaternion(const VectorX<double> StateRPY)
+    const {
+    VectorX<double> StateQ = VectorX<double>::Zero(13);
+
+    Vector4<double> angles_q = math::rpy2quat(
+        static_cast<Vector3<double>>(StateRPY.segment(3, 3)));
+
+    StateQ << StateRPY.segment(0, 3),
+        angles_q,
+        StateRPY.segment(9, 3),
+        StateRPY.segment(6, 3);
+    //[translation; quaternion; angular_velocity; linear_velocity]
+    return StateQ;
+  }
+};
 
 //  Combines test setup for both kinds of plants:
 //  ge_model_:  GenericQuadrotor
@@ -165,55 +240,79 @@ class QuadrotorTest: public ::testing::Test {
   QuadrotorTest() {
     ge_model_ = std::make_unique<GenericQuadrotor<double>>();
     rb_model_ = std::make_unique<RigidBodyQuadrotor<double>>();
-    ad_model_ = std::make_unique<RigidBodyAutoDiffQuadrotor<double>>();
+    ad_model_ = std::make_unique<RigidBodyAutoDiffRPYQuadrotor<double>>();
+    qa_model_ = std::make_unique<RigidBodyAutoDiffQuaternionQuadrotor<double>>();
 
     ge_simulator_ = std::make_unique<systems::Simulator<double>>(*ge_model_);
     rb_simulator_ = std::make_unique<systems::Simulator<double>>(*rb_model_);
     ad_simulator_ = std::make_unique<systems::Simulator<double>>(*ad_model_);
+    qa_simulator_ = std::make_unique<systems::Simulator<double>>(*qa_model_);
 
     ge_derivatives_ = ge_model_->AllocateTimeDerivatives();
     rb_derivatives_ = rb_model_->AllocateTimeDerivatives();
     ad_derivatives_ = ad_model_->AllocateTimeDerivatives();
+    qa_derivatives_ = qa_model_->AllocateTimeDerivatives();
   }
 
   void SetUp() override {
     ge_context_ = ge_model_->CreateDefaultContext();
     rb_context_ = rb_model_->CreateDefaultContext();
     ad_context_ = ad_model_->CreateDefaultContext();
+    qa_context_ = qa_model_->CreateDefaultContext();
 
     ge_model_->SetState(ge_context_.get(), x0_);
     rb_model_->SetState(rb_context_.get(), x0_);
     ad_model_->SetState(ad_context_.get(), x0_);
+    qa_model_->SetState(qa_context_.get(), x0_);
 
     ge_simulator_->Initialize();
     rb_simulator_->Initialize();
     ad_simulator_->Initialize();
+    qa_simulator_->Initialize();
   }
 
   void SetState(const VectorX<double> x0) {
     ge_context_ = ge_model_->CreateDefaultContext();
     rb_context_ = rb_model_->CreateDefaultContext();
     ad_context_ = ad_model_->CreateDefaultContext();
+    qa_context_ = qa_model_->CreateDefaultContext();
 
     ge_model_->SetState(ge_context_.get(), x0);
     rb_model_->SetState(rb_context_.get(), x0);
     ad_model_->SetState(ad_context_.get(), x0);
+    qa_model_->SetState(qa_context_.get(), x0);
   }
 
   void Simulate(const double t) {
     ge_simulator_->Initialize();
     rb_simulator_->Initialize();
     ad_simulator_->Initialize();
+    qa_simulator_->Initialize();
 
     ge_simulator_->StepTo(t);
     rb_simulator_->StepTo(t);
     ad_simulator_->StepTo(t);
+    qa_simulator_->StepTo(t);
   }
 
   VectorX<double> GetState(systems::Simulator<double> *simulator) {
     return simulator->get_context()
         .get_continuous_state_vector()
         .CopyToVector();
+  }
+
+  static VectorX<double> ConvertQuaternionStateToRPY(const VectorX<double> StateQ) {
+    VectorX<double> StateRPY = VectorX<double>::Zero(12);
+
+    Vector3<double> angles_RPY = math::quat2rpy(
+        static_cast<Vector4<double>>(StateQ.segment(3, 4)));
+
+    StateRPY << StateQ.segment(0, 3),
+        angles_RPY,
+        StateQ.segment(10, 3),
+        StateQ.segment(7, 3);
+    //[translation; quaternion; angular_velocity; linear_velocity]
+    return StateRPY;
   }
 
   void PassiveBehaviorTest(VectorX<double> x0) {
@@ -228,11 +327,17 @@ class QuadrotorTest: public ::testing::Test {
     VectorX<double> ad_state = ad_simulator_->get_context()
         .get_continuous_state_vector()
         .CopyToVector();
+    VectorX<double> qa_state_quaternion = qa_simulator_->get_context()
+        .get_continuous_state_vector()
+        .CopyToVector();
+    VectorX<double> qa_state = ConvertQuaternionStateToRPY(qa_state_quaternion);
     double tol = 1e-10;
     EXPECT_TRUE(
         CompareMatrices(my_state, rb_state, tol, MatrixCompareType::absolute));
     EXPECT_TRUE(
         CompareMatrices(my_state, ad_state, tol, MatrixCompareType::absolute));
+    EXPECT_TRUE(
+        CompareMatrices(my_state, qa_state, tol, MatrixCompareType::absolute));
   }
 
  protected:
@@ -240,15 +345,16 @@ class QuadrotorTest: public ::testing::Test {
 
   std::unique_ptr<GenericQuadrotor<double>> ge_model_;
   std::unique_ptr<RigidBodyQuadrotor<double>> rb_model_;
-  std::unique_ptr<RigidBodyAutoDiffQuadrotor<double>> ad_model_;
+  std::unique_ptr<RigidBodyAutoDiffRPYQuadrotor<double>> ad_model_;
+  std::unique_ptr<RigidBodyAutoDiffQuaternionQuadrotor<double>> qa_model_;
 
   std::unique_ptr<systems::Simulator<double>> ge_simulator_, rb_simulator_,
-      ad_simulator_;
+      ad_simulator_, qa_simulator_;
 
   std::unique_ptr<systems::Context<double>> ge_context_, rb_context_,
-      ad_context_;
+      ad_context_, qa_context_;
   std::unique_ptr<systems::ContinuousState<double>> ge_derivatives_,
-      rb_derivatives_, ad_derivatives_;
+      rb_derivatives_, ad_derivatives_, qa_derivatives_;
 };
 
 //  Test comparing the computation of derivatives for a fixed state.
@@ -259,15 +365,20 @@ TEST_F(QuadrotorTest, derivatives) {
   ge_model_->CalcTimeDerivatives(*ge_context_, ge_derivatives_.get());
   rb_model_->CalcTimeDerivatives(*rb_context_, rb_derivatives_.get());
   ad_model_->CalcTimeDerivatives(*ad_context_, ad_derivatives_.get());
+  qa_model_->CalcTimeDerivatives(*qa_context_, qa_derivatives_.get());
 
   VectorX<double> my_derivative_vector = ge_derivatives_->CopyToVector();
   VectorX<double> rb_derivative_vector = rb_derivatives_->CopyToVector();
   VectorX<double> ad_derivative_vector = ad_derivatives_->CopyToVector();
+  VectorX<double> qa_derivative_vector = qa_derivatives_->CopyToVector();
 
   EXPECT_TRUE(CompareMatrices(my_derivative_vector, rb_derivative_vector,
                               1e-10 /* tolerance */,
                               MatrixCompareType::absolute));
   EXPECT_TRUE(CompareMatrices(my_derivative_vector, ad_derivative_vector,
+                              1e-10 /* tolerance */,
+                              MatrixCompareType::absolute));
+  EXPECT_TRUE(CompareMatrices(my_derivative_vector, qa_derivative_vector,
                               1e-10 /* tolerance */,
                               MatrixCompareType::absolute));
 }
