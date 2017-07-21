6a4d31ddb484d00f2e812b5821378191d465a408
robinsch <robin.scherrer@gmail.com>
2017-07-21 14:16:09 +0200

    test for linearization works

--- drake/examples/Quadrotor/test/quadrotor_linearization_test.cc
+++ drake/examples/Quadrotor/test/quadrotor_linearization_test.cc
@@ -26,24 +26,36 @@ namespace examples {
 namespace quadrotor {
 namespace {
 
-const double equilibrium_check_tolerance = 1;
+const double equilibrium_check_tolerance = 1e3;
+const double g = 9.81,  // Gravitational acceleration (m/s^2).
+    m = 0.5;            // Mass of the robot (kg).
+// The nominal hover position is at (0, 0, 1.0) in world coordinates.
+const Eigen::Vector3d kNominalPosition{((Eigen::Vector3d() << 0.0, 0.0, 1.0).
+    finished())};
 
-// A Generic Quadrotor Plant Diagram with the plant created from
-// QuadrotorPlant.
-template<typename T>
-class GenericQuadrotor {
+// A Generic Quadrotor Plant with the plant created from QuadrotorPlant.
+template <typename T>
+class GenericQuadrotor: public systems::Diagram<T> {
  public:
-  GenericQuadrotor() {
-
+  GenericQuadrotor(VectorX<T> input) {
+    this->set_name("QuadrotorTest");
     systems::DiagramBuilder<double> builder;
 
-    plant_ = builder.AddSystem<QuadrotorPlant<double>>();
+    plant_ = builder.template AddSystem<QuadrotorPlant<double>>();
     plant_->set_name("plant");
     context_ = plant_->CreateDefaultContext();
+    context_->FixInputPort(0, input);
+
+    systems::ConstantVectorSource<T>* source =
+        builder.template AddSystem<systems::ConstantVectorSource<T>>(input);
+    source->set_name("input");
+
+    builder.Connect(source->get_output_port(), plant_->get_input_port(0));
+
+    builder.BuildInto(this);
   }
 
   void SetState(const VectorX<T>& x0, const VectorX<T>& u0) {
-    context_->FixInputPort(0, u0);
     plant_->set_state(context_.get(), x0);
   }
 
@@ -63,16 +75,15 @@ class GenericQuadrotor {
  private:
   QuadrotorPlant<T> *plant_{};
   std::unique_ptr<systems::Context<double>> context_;
-
 };
 
-
-//  A Quadrotor as a RigidBodyPlant that is created from a model
-// specified in a URDF file using a Euler angles.
-template<typename T>
-class RigidBodyAutoDiffRPYQuadrotor {
+// A Quadrotor as a RigidBodyPlantAutodiff that is created from a model
+// specified in a URDF file using Euler angles.
+template <typename T>
+class RigidBodyAutoDiffRPYQuadrotor: public systems::Diagram<T> {
  public:
   RigidBodyAutoDiffRPYQuadrotor() {
+    this->set_name("QuadrotorAD");
 
     systems::DiagramBuilder<AutoDiffXd> builder;
 
@@ -88,68 +99,82 @@ class RigidBodyAutoDiffRPYQuadrotor {
     plant_ =
         builder.template AddSystem<systems::RigidBodyPlantAutodiff<AutoDiffXd>>(
             std::move(treead));
+    plant_->set_name("plantAD");
 
+    builder.BuildInto(this);
   }
 
-  std::vector<Eigen::MatrixXd> getABMatrices(const VectorX<T>& x0,
-                                             const VectorX<T>& u0) {
+  std::vector<Eigen::MatrixXd> getABMatrices(const VectorX<double>& x0,
+                                             const VectorX<double>& u0) {
     std::vector<Eigen::MatrixXd> ABMatrices;
     ABMatrices = plant_->LinearizeAB(x0, u0);
 
     return ABMatrices;
   }
+
  private:
-  systems::RigidBodyPlantAutodiff<T> *plant_{};
+  systems::RigidBodyPlantAutodiff<AutoDiffXd> *plant_{};
 };
 
-class QuadrotorTest: public ::testing::Test {
- public:
-  QuadrotorTest() {
-    ge_model_ = std::make_unique<GenericQuadrotor<double>>();
-    ad_model_ = std::make_unique<RigidBodyAutoDiffRPYQuadrotor<double>>();
-
-  }
-
-  void BehaviorTest(VectorX<double> x0, VectorX<double> u0) {
-    std::vector<Eigen::MatrixXd> ge_AB = ge_model_->getABMatrices(x0, u0);
-    std::vector<Eigen::MatrixXd> ad_AB = ad_model_->getABMatrices(x0, u0);
-
-    double tol = 1e-10;
-    EXPECT_TRUE(
-        CompareMatrices(ge_AB[0], ad_AB[0], tol, MatrixCompareType::absolute));
-    EXPECT_TRUE(
-        CompareMatrices(ge_AB[1], ad_AB[1], tol, MatrixCompareType::absolute));
-  }
-
-  const double g_{9.81},  // Gravitational acceleration (m/s^2).
-      m_{0.5};            // Mass of the robot (kg).
-
-
- protected:
-  VectorX<double> x0_ = VectorX<double>::Zero(12);
-
-  std::unique_ptr<GenericQuadrotor<double>> ge_model_;
-  std::unique_ptr<RigidBodyAutoDiffRPYQuadrotor<double>> ad_model_;
-
-  std::unique_ptr<systems::Context<double>> ge_context_;
-
-
-};
-
-TEST_F(QuadrotorTest, hover) {
-
-  // The nominal hover position is at (0, 0, 1.0) in world coordinates.
-  const Eigen::Vector3d kNominalPosition{((Eigen::Vector3d() << 0.0, 0.0, 1.0).
-      finished())};
-
+GTEST_TEST(TestQuadrotorPlantLinearize, hover) {
   Eigen::VectorXd x0 = Eigen::VectorXd::Zero(12);
-  x0.topRows(3) = kNominalPosition;
+  x0.topRows(3) << kNominalPosition;
 
-// Nominal input corresponds to a hover.
-  Eigen::VectorXd u0 = Eigen::VectorXd::Constant(
-      4, m_ * g_ / 4);
+  // Nominal input corresponds to a hover.
+  const Eigen::VectorXd u0 = Eigen::VectorXd::Constant(4, m * g / 4);
 
-  BehaviorTest(x0, u0);
+  auto ge_model_ = std::make_unique<GenericQuadrotor<double>>(u0);
+  auto ad_model_ =
+      std::make_unique<RigidBodyAutoDiffRPYQuadrotor<AutoDiffXd>>();
+
+  std::vector<Eigen::MatrixXd> ge_AB = ge_model_->getABMatrices(x0, u0);
+  std::vector<Eigen::MatrixXd> ad_AB = ad_model_->getABMatrices(x0, u0);
+
+  double tol = 1e-10;
+  EXPECT_TRUE(
+      CompareMatrices(ge_AB[0], ad_AB[0], tol, MatrixCompareType::absolute));
+  EXPECT_TRUE(
+      CompareMatrices(ge_AB[1], ad_AB[1], tol, MatrixCompareType::absolute));
+}
+GTEST_TEST(TestQuadrotorPlantLinearize, zero) {
+  Eigen::VectorXd x0 = Eigen::VectorXd::Zero(12);
+  x0.topRows(3) << kNominalPosition;
+
+  const Eigen::VectorXd u0 = Eigen::VectorXd::Constant(4, 0);
+
+  auto ge_model_ = std::make_unique<GenericQuadrotor<double>>(u0);
+  auto ad_model_ =
+      std::make_unique<RigidBodyAutoDiffRPYQuadrotor<AutoDiffXd>>();
+
+  std::vector<Eigen::MatrixXd> ge_AB = ge_model_->getABMatrices(x0, u0);
+  std::vector<Eigen::MatrixXd> ad_AB = ad_model_->getABMatrices(x0, u0);
+
+  double tol = 1e-10;
+  EXPECT_TRUE(
+      CompareMatrices(ge_AB[0], ad_AB[0], tol, MatrixCompareType::absolute));
+  EXPECT_TRUE(
+      CompareMatrices(ge_AB[1], ad_AB[1], tol, MatrixCompareType::absolute));
+}
+
+GTEST_TEST(TestQuadrotorPlantLinearize, random) {
+  Eigen::VectorXd x0 = Eigen::VectorXd::Zero(12);
+  x0 << 1, 2, 3, 0.4, 0.5, 0.6, 1, 2, 3, 4, 5, 6;
+
+  Eigen::VectorXd u0 = Eigen::VectorXd::Constant(4, 0);
+  u0 << 0.6, 0.9, 1.2, 1.0;
+
+  auto ge_model_ = std::make_unique<GenericQuadrotor<double>>(u0);
+  auto ad_model_ =
+      std::make_unique<RigidBodyAutoDiffRPYQuadrotor<AutoDiffXd>>();
+
+  std::vector<Eigen::MatrixXd> ge_AB = ge_model_->getABMatrices(x0, u0);
+  std::vector<Eigen::MatrixXd> ad_AB = ad_model_->getABMatrices(x0, u0);
+
+  double tol = 1e-10;
+  EXPECT_TRUE(
+      CompareMatrices(ge_AB[0], ad_AB[0], tol, MatrixCompareType::absolute));
+  EXPECT_TRUE(
+      CompareMatrices(ge_AB[1], ad_AB[1], tol, MatrixCompareType::absolute));
 }
 
 }  // namespace
